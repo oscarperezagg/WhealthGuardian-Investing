6b614792ac8c28f49233367a97a891e0d484d5f3
Oscar Perez Arruti <oscarperez.agg@gmail.com>
2023-08-28 20:41:04 +0200

    Added Preproduction service

---
+++ app/util/PREPRODUCTION/preproduction.py
@@ -0,0 +1,3 @@
+# This file contains all the functions that are used in the preproduction process, like the creation of the
+# preproduction folder, the creation of the preproduction files, the creation of the preproduction folder, the
+# creation of the preproduction database
