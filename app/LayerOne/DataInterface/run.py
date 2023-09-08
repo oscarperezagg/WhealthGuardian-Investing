2c6385a41e31976211a841992327c64d891fe17e
Oscar Perez Arruti <oscarperez.agg@gmail.com>
2023-09-08 18:25:36 +0200

    Comments About File

--- app/LayerOne/DataInterface/run.py
+++ app/LayerOne/DataInterface/run.py
@@ -1,8 +1,15 @@
 from jobs import TDA_ReferenceData
 
+
+
+"""
+
+Esto se deberá schedulear para que se ejecute cada día una vez.
+
 TDA_ReferenceData.getStocksList()
 TDA_ReferenceData.getIndicesList()
-
 TDA_ReferenceData.getForexList()
 TDA_ReferenceData.getCryptocurrenciesList()
 TDA_ReferenceData.getExchangesList()
+
+"""
