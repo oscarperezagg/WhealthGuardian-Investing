1aa4c7719d2e75e7dd5a76e8cdceca08aa6101b0
Oscar Perez Arruti <oscarperez.agg@gmail.com>
2023-09-18 00:00:35 +0200

    AlphaVantaje new functions and TwelveDataAPI functions

---
+++ app/LayerThree/DataInterface/secret.py
@@ -0,0 +1,13 @@
+ALPHA_VANTAGE_API_KEY = "4876J3YVNCWI1M3E"
+
+TWELVE_DATA_API_KEY = "23820da0301e466d89d800ac6329c07f"
+
+# This needs to go inside the database 
+DATABASE = {
+    "host": "192.168.1.76",
+    "port": "27017",
+    "username": None,
+    "password": None,
+    "dbname": "WhealthGuardian",
+}
+
